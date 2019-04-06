38d4ec6227cc0ad8ed00bd39ec707f175643427f
Rohan <rohanp9000@gmail.com>
2019-04-06 19:25:23 -0400

    Added command group and individual commands for hatch placement with arm and elevator integration.

---
+++ src/main/java/org/usfirst/frc5422/Minimec/commands/Elevator/ElevatorPlacement.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc5422.Minimec.commands.Elevator;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc5422.Minimec.Robot;
+import org.usfirst.frc5422.utils.StormProp;
+
+public class ElevatorPlacement extends Command {
+
+    public ElevatorPlacement(){
+        if (Robot.useElevator){requires(Robot.elevator);}
+    }
+
+    protected void execute(){
+       Robot.elevator.moveToTicks(StormProp.getInt("elevatorPlacementPosition", 0));
+    }
+    protected void initialize(){
+        System.out.println("ElevatorMove.initialize() : " + StormProp.getInt("elevatorPlacementPosition", 0));
+    }
+    protected void interrupted(){
+        System.out.println("ElevatorMove.interrupted()");
+        Robot.elevator.stop();}
+
+    protected void end(){
+        System.out.println("ElevatorMove.end()");
+        Robot.elevator.stop();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return Robot.elevator.isFinished();
+    }
+}
---
+++ src/main/java/org/usfirst/frc5422/Minimec/commands/Elevator/PlacementSequence.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc5422.Minimec.commands.Elevator;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc5422.Minimec.Robot;
+import org.usfirst.frc5422.Minimec.commands.Arm.ArmPlacement;
+
+public class PlacementSequence extends CommandGroup {
+
+
+    public PlacementSequence(){
+        addParallel(new ArmPlacement());
+        addParallel(new ElevatorPlacement());
+    }
+
+
+
+}
