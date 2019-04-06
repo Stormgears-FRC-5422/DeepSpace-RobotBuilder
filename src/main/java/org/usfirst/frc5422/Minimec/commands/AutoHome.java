b3cf9a1bc568cdb2fee41fdf79d8f34806eb9650
kaanay03 <kaanay03@gmail.com>
2019-04-06 13:52:52 -0400

    added navx reset to homing

--- src/main/java/org/usfirst/frc5422/Minimec/commands/AutoHome.java
+++ src/main/java/org/usfirst/frc5422/Minimec/commands/AutoHome.java
@@ -4,12 +4,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc5422.Minimec.Robot;
 import org.usfirst.frc5422.Minimec.commands.Arm.ArmReset;
 import org.usfirst.frc5422.Minimec.commands.Elevator.ElevatorReset;
+import org.usfirst.frc5422.Minimec.commands.Gyro.NavXReset;
 import org.usfirst.frc5422.Minimec.commands.Jack.BackjackReset;
 
 public class AutoHome extends CommandGroup {
     public AutoHome(boolean active) {
         System.out.println("Starting auto-homing sequence - active = " + active);
         // Note that these aren't run here - they are created and scheduled. Running happens next
+        if (Robot.useNavX) addSequential(new NavXReset(active));
         if (Robot.useArm) addSequential(new ArmReset(active));
         if (Robot.useElevator) addSequential(new ElevatorReset(active));
         if (Robot.useBackjack) addSequential(new BackjackReset(active));
