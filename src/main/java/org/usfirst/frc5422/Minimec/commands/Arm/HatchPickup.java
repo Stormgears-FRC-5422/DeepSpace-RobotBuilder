7169cd33849f4483dfe89d885be945ceeb8bac36
Andrew Barber <git@commerce.abarber.net>
2019-04-17 00:57:06 +0100

    Added forgotten command

---
+++ src/main/java/org/usfirst/frc5422/Minimec/commands/Arm/HatchPickup.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc5422.Minimec.commands.Arm;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc5422.Minimec.Robot;
+import org.usfirst.frc5422.Minimec.commands.Arm.*;
+import org.usfirst.frc5422.Minimec.commands.Elevator.*;
+import org.usfirst.frc5422.Minimec.commands.Pneumatics.*;
+
+public class HatchPickup extends CommandGroup {
+
+    public HatchPickup(){
+        System.out.println("Starting hatch pickup sequence command");
+        if (Robot.useElevator) addSequential(new ElevatorReset(true));
+        if (Robot.useArm) addSequential(new ArmToPosition(Robot.arm.ARM_PICKUP_POSITION_TICKS));
+        if (Robot.useValveControl) addSequential(new HatchVacEnable(true));
+        if (Robot.useArm) addSequential(new ArmToPosition(Robot.arm.ARM_REST_POSITION_TICKS));
+    }
+
+
+}
